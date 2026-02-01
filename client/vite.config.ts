b034d39ebc58310fc5235265d434a6353657e925
redarasmy <red7mer@gmail.com>
2026-02-01 17:45:17 +0100

    chore: add vite server proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,10 +5,18 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://localhost:3000",
+                changeOrigin: true,
+            },
+        },
+    },
 })
