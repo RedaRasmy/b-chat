c8732af69b5741b884ea14f3dd5863d886e9caa3
redarasmy <red7mer@gmail.com>
2026-02-01 18:12:04 +0100

    feat: add react-query

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,14 @@ import { RouterProvider } from "react-router-dom"
 import "./index.css"
 import { router } from "@/routes.tsx"
 import { AuthProvider } from "@/features/auth/provider"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const client = new QueryClient()
 
 createRoot(document.getElementById("root")!).render(
-    <AuthProvider>
-        <RouterProvider router={router} />
-    </AuthProvider>,
+    <QueryClientProvider client={client}>
+        <AuthProvider>
+            <RouterProvider router={router} />
+        </AuthProvider>
+    </QueryClientProvider>,
 )
