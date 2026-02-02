7d6aaecb6c9ce1c412f965a4a3bccb38232be960
redarasmy <red7mer@gmail.com>
2026-02-02 13:41:08 +0100

    feat: protect pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,17 @@
 import { AppSidebar } from "@/components/app-sidebar"
 import { SidebarProvider } from "@/components/ui/sidebar"
-import { Outlet } from "react-router-dom"
+import { useAuth } from "@/features/auth/use-auth"
+import LoadingPage from "@/pages/loading"
+import { Navigate, Outlet } from "react-router-dom"
 
 export function App() {
+    const { isAuthenticated, isLoading } = useAuth()
+    if (isLoading) return <LoadingPage />
+
+    if (!isAuthenticated) {
+        return <Navigate to={"/auth/login"} replace />
+    }
+
     return (
         <SidebarProvider>
             <AppSidebar />
--- client/src/pages/auth/index.tsx
+++ client/src/pages/auth/index.tsx
@@ -1,5 +1,14 @@
-import { Outlet } from "react-router-dom"
+import { useAuth } from "@/features/auth/use-auth"
+import LoadingPage from "@/pages/loading"
+import { Navigate, Outlet } from "react-router-dom"
 
 export default function AuthLayout() {
+    const { isAuthenticated, isLoading } = useAuth()
+    if (isLoading) return <LoadingPage />
+
+    if (isAuthenticated) {
+        return <Navigate to={"/"} replace />
+    }
+
     return <Outlet />
 }
