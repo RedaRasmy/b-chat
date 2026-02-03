ddc8b3b7aee5b163c39a0bbe8c236b7710580276
redarasmy <red7mer@gmail.com>
2026-02-03 23:24:23 +0100

    feat: add posts validation schemas

---
+++ packages/validation/src/auth.ts
@@ -0,0 +1,24 @@
+import { z } from "zod"
+
+export const LoginSchema = z.object({
+    email: z.email(),
+    password: z
+        .string()
+        .min(1, "Password is required")
+        .min(8, "Password must be between 8 and 50 characters")
+        .max(50, "Password must be between 8 and 50 characters"),
+})
+
+export const RegisterSchema = z.object({
+    name: z
+        .string()
+        .min(1, "Name is required")
+        .min(3, "Name must be between 3 and 30 characters")
+        .max(30, "Name must be between 3 and 30 characters"),
+    email: z.email(),
+    password: z
+        .string()
+        .min(1, "Password is required")
+        .min(8, "Password must be between 8 and 50 characters")
+        .max(50, "Password must be between 8 and 50 characters"),
+})
---
+++ packages/validation/src/posts.ts
@@ -0,0 +1,21 @@
+import { posts } from "@bchat/database/tables"
+import {
+    createInsertSchema,
+    createSelectSchema,
+    createUpdateSchema,
+} from "drizzle-zod"
+import z from "zod"
+
+export const InsertPostSchema = createInsertSchema(posts).pick({
+    content: true,
+})
+
+export const SelectPostSchema = createSelectSchema(posts)
+
+export const UpdatePostSchema = createUpdateSchema(posts).pick({
+    content: true,
+})
+
+export type IPost = z.infer<typeof InsertPostSchema>
+export type SPost = z.infer<typeof SelectPostSchema>
+export type UPost = z.infer<typeof UpdatePostSchema>
