3ccdac4640794757598d14e11912b8738ef394f3
redarasmy <red7mer@gmail.com>
2026-02-04 09:49:21 +0100

    feat: add comments validation schemas

---
+++ packages/validation/src/comments.ts
@@ -0,0 +1,21 @@
+import { comments } from "@bchat/database/tables"
+import {
+    createInsertSchema,
+    createSelectSchema,
+    createUpdateSchema,
+} from "drizzle-zod"
+import z from "zod"
+
+export const InsertCommentSchema = createInsertSchema(comments).pick({
+    content: true,
+})
+
+export const SelectCommentSchema = createSelectSchema(comments)
+
+export const UpdateCommentSchema = createUpdateSchema(comments).pick({
+    content: true,
+})
+
+export type IComment = z.infer<typeof InsertCommentSchema>
+export type SComment = z.infer<typeof SelectCommentSchema>
+export type UComment = z.infer<typeof UpdateCommentSchema>
