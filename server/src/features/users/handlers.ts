fd808d3c1739cdb182bc3457f913706b6ab9578f
redarasmy <red7mer@gmail.com>
2026-02-05 16:29:50 +0100

    fix: exclude the user from the response

--- server/src/features/users/handlers.ts
+++ server/src/features/users/handlers.ts
@@ -1,17 +1,23 @@
 import { makeQueryEndpoint } from "@/utils/wrappers"
 import db from "@bchat/database"
-import { users } from "@bchat/database/tables"
 import { QueryUsersSchema } from "@bchat/shared/validation"
 
 export const getUsers = makeQueryEndpoint(
     QueryUsersSchema,
     async (req, res, next) => {
         const { search } = req.validatedQuery
+        const user = req.user!
 
         try {
             const data = await db.query.users.findMany({
-                where: (users, { ilike }) => {
-                    if (search) return ilike(users.name, search)
+                where: (users, { ilike, and, ne }) => {
+                    if (search)
+                        return and(
+                            ne(users.id, user.id),
+                            ilike(users.name, search),
+                        )
+
+                    return ne(users.id, user.id)
                 },
                 columns: {
                     id: true,
