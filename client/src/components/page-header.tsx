cb97320b2b2a12e21f762b2351ba974cc72061ae
redarasmy <red7mer@gmail.com>
2026-02-07 10:35:53 +0100

    feat: add PageHeader component

---
+++ client/src/components/page-header.tsx
@@ -0,0 +1,13 @@
+import { SidebarTrigger } from "@/components/ui/sidebar"
+import type { ReactNode } from "react"
+
+export default function PageHeader({ children }: { children: ReactNode }) {
+    return (
+        <header className="bg-accent h-12 flex items-center px-3 gap-3 border-b">
+            <SidebarTrigger size={"lg"} />
+            <div className="flex justify-between items-center w-full">
+                {children}
+            </div>
+        </header>
+    )
+}
