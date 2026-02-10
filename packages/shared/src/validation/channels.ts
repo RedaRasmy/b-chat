ff905ff73613318dd8750bdabf78b98387d58db1
redarasmy <red7mer@gmail.com>
2026-02-10 15:16:38 +0100

    feat: add groups validation schema

--- packages/shared/src/validation/channels.ts
+++ packages/shared/src/validation/channels.ts
@@ -1,11 +1,18 @@
-// import { channels } from "@bchat/database/tables"
 import z from "zod"
 
 export const InsertDMSchema = z.object({
     friendId: z.uuid(),
 })
+export const InsertGroupSchema = z.object({
+    name: z
+        .string()
+        .min(3, "Name length must be between 3 and 20")
+        .max(20, "Name length must be between 3 and 20"),
+    members: z.array(z.uuid()),
+})
 
 export type DMFormData = z.infer<typeof InsertDMSchema>
+export type GroupFormData = z.infer<typeof InsertGroupSchema>
 
 export const NewDMSchema = z.object({
     channelId: z.uuid(),
