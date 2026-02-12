d31f5a4606424ccd0f812145b32fafd1542716ac
redarasmy <red7mer@gmail.com>
2026-02-12 15:28:02 +0100

    feat: add smarter dates in chat cards and messages

--- client/src/features/chats/utils/get-time.ts
+++ client/src/features/chats/utils/get-time.ts
@@ -1,6 +1,17 @@
-export function getTime(timestamp: Date) {
-    return new Date(timestamp).toLocaleTimeString([], {
-        hour: "2-digit",
-        minute: "2-digit",
-    })
+import { isToday, isYesterday, isThisWeek, format } from "date-fns"
+
+export function getTime(date: Date | string | number) {
+    if (isToday(date)) {
+        return format(date, "HH:mm")
+    }
+
+    if (isYesterday(date)) {
+        return "Yesterday"
+    }
+
+    if (isThisWeek(date, { weekStartsOn: 1 })) {
+        return format(date, "EEEE")
+    }
+
+    return format(date, "d/M/yyyy") // "12/1/2026"
 }
