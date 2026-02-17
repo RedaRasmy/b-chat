a02b77f1b1d681fa64674ba51e3113129e1b3787
redarasmy <red7mer@gmail.com>
2026-02-17 17:57:55 +0000

    feat: add useSocketListener

---
+++ client/src/features/chats/hooks/use-socket-listener.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useSocket } from "@/features/chats/use-socket"
+import type { ServerEvent, ServerPayloads } from "@bchat/shared/events"
+import { useEffect, useLayoutEffect, useRef } from "react"
+
+export function useSocketListener<T extends ServerEvent>(
+    event: T,
+    handler: (data: ServerPayloads[T]) => void,
+) {
+    const socket = useSocket()
+
+    const handlerRef = useRef(handler)
+    useLayoutEffect(() => {
+        handlerRef.current = handler
+    })
+
+    useEffect(() => {
+        if (!socket) return
+
+        function stableHandler(data: ServerPayloads[T]) {
+            handlerRef.current(data)
+        }
+
+        socket.on(event as any, stableHandler as any)
+
+        return () => {
+            socket.off(event as any, stableHandler as any)
+        }
+    }, [socket, event])
+}
