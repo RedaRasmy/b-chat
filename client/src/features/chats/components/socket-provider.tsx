4a8dab2b8dbfee04eee127f36cb9c577fd3b4d5b
reda <red7mer@gmail.com>
2026-07-03 15:38:22 +0100

    refactor: write the event-listeners inside useEffect

--- client/src/features/chats/components/socket-provider.tsx
+++ client/src/features/chats/components/socket-provider.tsx
@@ -9,31 +9,27 @@ import type {
 export type ClientSocket = Socket<ServerToClientEvents, ClientToServerEvents>
 
 export default function SocketProvider({ children }: { children: ReactNode }) {
-    const [socket] = useState<ClientSocket>(() => {
-        const newSocket = io(
-            import.meta.env.DEV ? import.meta.env.VITE_API_URL : "/",
-            {
-                withCredentials: true,
-            },
-        )
-
-        newSocket.on("connect", () => {
-            console.log("Connected")
-        })
-
-        newSocket.on("disconnect", (reason) => {
-            console.warn("Disconnected:", reason)
-        })
-
-        newSocket.on("connect_error", (error) => {
-            console.error("Connection Error:", error)
-        })
-
-        return newSocket
-    })
+    const [socket] = useState<ClientSocket>(() =>
+        io(import.meta.env.DEV ? import.meta.env.VITE_API_URL : "/", {
+            withCredentials: true,
+        }),
+    )
 
     useEffect(() => {
+        const onConnect = () => console.log("Connected")
+        const onDisconnect = (reason: string) =>
+            console.warn("Disconnected:", reason)
+        const onConnectError = (error: Error) =>
+            console.error("Connection Error:", error)
+
+        socket.on("connect", onConnect)
+        socket.on("disconnect", onDisconnect)
+        socket.on("connect_error", onConnectError)
+
         return () => {
+            socket.off("connect", onConnect)
+            socket.off("disconnect", onDisconnect)
+            socket.off("connect_error", onConnectError)
             socket.disconnect()
         }
     }, [socket])
