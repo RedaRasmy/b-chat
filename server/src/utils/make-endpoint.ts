1e9718687b2b77711bf91a283d8ade4715677599
reda <red7mer@gmail.com>
2026-07-23 04:25:28 +0100

    feat: add user validation in makeEndpoint

--- server/src/utils/make-endpoint.ts
+++ server/src/utils/make-endpoint.ts
@@ -1,19 +1,29 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { AccessTokenPayload } from "@/lib/jwt"
+import logger from "@/lib/logger"
 import type { NextFunction, Request, Response } from "express"
 import z from "zod"
 
-export function makeEndpoint<Params, Query, Body>(
+type User<U extends boolean> = U extends true
+    ? AccessTokenPayload
+    : U extends false
+      ? undefined
+      : AccessTokenPayload | undefined
+
+export function makeEndpoint<Params, Query, Body, U extends boolean>(
     {
         params,
         query,
         body,
+        user,
     }: {
         params?: z.ZodType<Params>
         query?: z.ZodType<Query>
         body?: z.ZodType<Body>
+        user?: U
     },
     callback: (
-        req: Request<Params, any, Body, Query>,
+        req: Request<Params, any, Body, Query> & { user: User<U> },
         res: Response,
         next: NextFunction,
     ) => void,
@@ -23,16 +33,17 @@ export function makeEndpoint(
     callback: (req: Request, res: Response, next: NextFunction) => void,
 ): (req: Request, res: Response, next: NextFunction) => void
 
-export function makeEndpoint<Params, Query, Body>(
+export function makeEndpoint<Params, Query, Body, U extends boolean>(
     schemaOrCallback:
         | {
               params?: z.ZodType<Params>
               query?: z.ZodType<Query>
               body?: z.ZodType<Body>
+              user?: U
           }
         | ((req: Request, res: Response, next: NextFunction) => void),
     callback?: (
-        req: Request<Params, any, Body, Query>,
+        req: Request<Params, any, Body, Query> & { user: User<U> },
         res: Response,
         next: NextFunction,
     ) => void,
@@ -41,7 +52,7 @@ export function makeEndpoint<Params, Query, Body>(
         if (typeof schemaOrCallback === "function") {
             return schemaOrCallback(req, res, next)
         }
-        const { body, params, query } = schemaOrCallback
+        const { body, params, query, user } = schemaOrCallback
         const handler = callback!
 
         if (body) {
@@ -83,7 +94,22 @@ export function makeEndpoint<Params, Query, Body>(
             req.params = result.data as any
         }
 
-        return handler(req as Request<Params, any, Body, Query>, res, next)
+        if (user) {
+            if (!req.user) {
+                logger.fatal(
+                    "req.user is undefined, you must use auth middleware",
+                )
+                return res.sendStatus(500)
+            }
+        }
+
+        return handler(
+            req as Request<Params, any, Body, Query> & {
+                user: User<U>
+            },
+            res,
+            next,
+        )
     }
 }
 
@@ -92,10 +118,13 @@ export function makeEndpoint<Params, Query, Body>(
 //         params: z.object({
 //             someId: z.coerce.number(),
 //         }),
+//         // user: false,
 //     },
 //     async (req, res, next) => {
 //         req.body
 //         req.query
 //         req.params.someId
+
+//         const user = req.user
 //     },
 // )
